693906162c6dd5b3d872f3b65bdd6fc0289c32d1
naurez <mdnaurezalam888@gmail.com>
2024-08-02 19:04:00 +0530

    my first maven project

---
+++ src/main/java/project2/mycalc3.java
@@ -0,0 +1,17 @@
+package project2;
+
+public class mycalc3 {
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	public int diff(int a, int b) {
+		return a-b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mycalc3 c=new mycalc3();
+		System.out.println("sum is"+c.sum(20, 10));
+		System.out.println("sum is"+c.diff(20, 10));
+	}
+
+}
