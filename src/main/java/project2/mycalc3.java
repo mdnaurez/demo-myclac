1251494cec3614f1167515c7910a28191e677fea
naurez <mdnaurezalam888@gmail.com>
2024-08-02 19:17:01 +0530

    mul() added

--- src/main/java/project2/mycalc3.java
+++ src/main/java/project2/mycalc3.java
@@ -7,11 +7,15 @@ public class mycalc3 {
 	public int diff(int a, int b) {
 		return a-b;
 	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		mycalc3 c=new mycalc3();
 		System.out.println("sum is"+c.sum(20, 10));
 		System.out.println("sum is"+c.diff(20, 10));
+		System.out.println("sum is"+c.mul(20, 10));
 	}
 
 }
