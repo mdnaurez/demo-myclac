028cdc251d6fef4c409f1e0a5537abe1ef340c51
naurez <mdnaurezalam888@gmail.com>
2024-08-03 09:20:33 +0530

    div()added

--- src/main/java/project2/mycalc3.java
+++ src/main/java/project2/mycalc3.java
@@ -10,12 +10,16 @@ public class mycalc3 {
 	public int mul(int a, int b) {
 		return a*b;
 	}
+	public int div(int a, int b) {
+		return a/b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		mycalc3 c=new mycalc3();
 		System.out.println("sum is"+c.sum(20, 10));
 		System.out.println("sum is"+c.diff(20, 10));
 		System.out.println("sum is"+c.mul(20, 10));
+		System.out.println("sum is"+c.div(20, 10));
 	}
 
 }
